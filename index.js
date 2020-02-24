becdac2646c9bded2d690c2f1496d83745e46395
EvanBell <876895046@qq.com>
2020-02-24 11:32:55 +0800

    edit port.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/upload/:key', cors(), function(req, res, next){
   })
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 })
